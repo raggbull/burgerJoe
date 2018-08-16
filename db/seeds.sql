bd42336c14580c9882e1c5d20dde1478e7230e6e
Joe Hoffmann <josephjohnhoffmann@gmail.com>
2018-08-16 00:51:41 -0500

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,6 @@
 
 USE burgers;
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Single Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Burger');
